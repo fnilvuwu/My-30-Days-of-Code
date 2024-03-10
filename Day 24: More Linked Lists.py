7a55f5bf4959ae07dd24682360c4d65850568022
fnilvuwu <mamatmks45@gmail.com>
2024-03-10 22:54:40 +0800

    add day 24

---
+++ Day 24: More Linked Lists.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self, head):
+        current = head
+        while current:
+            next_distinct = current.next
+            while next_distinct and next_distinct.data == current.data:
+                next_distinct = next_distinct.next
+            current.next = next_distinct
+            current = next_distinct
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
